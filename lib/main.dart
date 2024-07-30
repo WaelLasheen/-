5dca7481a94e7107dbef4d0505a7ed452bb51280
Wael Lasheen <lasheenwael9@gmail.com>
2024-07-30 09:42:08 +0300

    first commit farah

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("Welcome to Kiswa ,Wael!"),
+          child: Text("Welcome to Kiswa ,Farah!"),
         ),
       ),
     );
