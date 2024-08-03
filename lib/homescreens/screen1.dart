a1e0c20544b1be5d99b9398aa0744c4ec19c9850
farahosama <159581667+FarahOsama511@users.noreply.github.com>
2024-08-03 05:55:03 +0300

    homescreens

---
+++ lib/homescreens/screen1.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:kiswa/homescreens/screen2.dart';
+import 'package:kiswa/homescreens/screen3.dart';
+
+class screen1 extends StatefulWidget {
+  @override
+  State<screen1> createState() {
+    return articles();
+  }
+}
+
+class articles extends State<screen1> {
+  int selectindex = 0;
+  List<String> article = [
+    "assets/images/Frame 85.png",
+    "assets/images/Frame 86.png",
+    "assets/images/Frame 87.png",
+    "assets/images/Frame 88.png",
+    "assets/images/Frame 89.png"
+  ];
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Stack(
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.notifications_sharp,
+                      size: 35,
+                    )),
+                const Positioned(
+                    left: 23,
+                    top: 12,
+                    child: Icon(
+                      Icons.circle,
+                      color: Color(0xFF84BF60),
+                      size: 10,
+                    ))
+              ],
+            ),
+            CircleAvatar(
+              backgroundColor: Color(0XFF84BF60),
+              child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  )),
+            )
+          ],
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+            child: RichText(
+              text: const TextSpan(
+                  text: " أهلا",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30.0,
+                      fontFamily: "Roboto",
+                      fontWeight: FontWeight.w900),
+                  children: [
+                    TextSpan(
+                        text: "إسراء ",
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Color(0XFF84BF60),
+                            fontFamily: "Roboto",
+                            fontWeight: FontWeight.bold))
+                  ]),
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        selectindex = 0;
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => screen1()));
+                      });
+                    },
+                    child: Text(
+                      "مقالات التوعية",
+                      style: TextStyle(
+                          color: selectindex == 0
+                              ? Color(0xFF1F201C)
+                              : Color(0XFF848787),
+                          fontSize: 30),
+                    ),
+                  ),
+                  MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        selectindex = 1;
+                      });
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return screen2();
+                      }));
+                    },
+                    child: Text("إنجازات الشهر",
+                        style: TextStyle(
+                          color: selectindex == 1
+                              ? Color(0xFF1F201C)
+                              : Color(0XFF848787),
+                          fontSize: 30,
+                        )),
+                  ),
+                  MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        selectindex = 2;
+                      });
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => screen3()));
+                    },
+                    child: Text(
+                      "أبرز المتبرعين",
+                      style: TextStyle(
+                          color: selectindex == 2
+                              ? Color(0xFF1F201C)
+                              : Color(0XFF848787),
+                          fontSize: 30),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: article.length,
+                itemBuilder: (context, index) {
+                  return ClipRRect(
+                    child: Image.asset(
+                      article[index],
+                      fit: BoxFit.fill,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kiswa/homescreens/screen1.dart';
+import 'package:kiswa/homescreens/screen2.dart';
 import 'package:kiswa/splash/logo.dart';
 import 'package:kiswa/splash/page1.dart';
-
 import 'package:kiswa/splash/page2.dart';
 
 void main() {
@@ -14,9 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: "Cairo"),
-      debugShowCheckedModeBanner: false,
-      home: Logo(),
-    );
+        theme: ThemeData(fontFamily: "Roboto"),
+        debugShowCheckedModeBanner: false,
+        home: Logo());
   }
 }
