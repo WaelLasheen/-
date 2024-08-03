4ecf59a517d1cc2cc7cbaea55e395a9f4429a5d3
farahosama <159581667+FarahOsama511@users.noreply.github.com>
2024-08-03 19:55:21 +0300

    edit

--- lib/chats/chat1.dart
+++ lib/chats/chat1.dart
@@ -5,17 +5,32 @@ class chat1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/Frame_487-removebg-preview.png",
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "مؤسسه مرسال الخيرية",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.bold),
+              ),
             )
           ],
         ),
-      ),
-    ));
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/Frame_487-removebg-preview.png",
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
